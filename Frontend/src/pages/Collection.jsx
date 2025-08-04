7d95caba8c93ee86c29747e65047dd6c81a5e686
AniketShirou <aniketranawork@gmail.com>
2025-08-04 23:52:16 +0530

    Search Feature

--- Frontend/src/pages/Collection.jsx
+++ Frontend/src/pages/Collection.jsx
@@ -13,7 +13,7 @@ const options = [
 
 const Collection = () => {
 
- const {products} = useContext(ShopContext); 
+ const {products , search , showSearch} = useContext(ShopContext); 
  const [showFilter , setShowFilter] = useState(false);
  const [filterProducts , setFilterProducts] = useState([]);
  const [category, setCategory] = useState([]);
@@ -44,6 +44,10 @@ const Collection = () => {
 //  function for displaying individual product
  const applyFilter = () =>{
   let  productsCopy = products.slice();
+
+  if(showSearch && search){
+    productsCopy = productsCopy.filter(item => item.name.toLowerCase().includes(search.toLowerCase()))
+  }
 
   if(category.length > 0){
     productsCopy = productsCopy.filter(item => category.includes(item.category))
@@ -76,7 +80,7 @@ const Collection = () => {
 
   useEffect(()=>{
     applyFilter();
- },[category , subCategory])
+ },[category , subCategory, search, showSearch])
 
  useEffect(() => {
   sortProduct();
