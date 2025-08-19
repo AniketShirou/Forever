c06ae3c5b1f1c69f811030c7cd3b2255a3d82cf3
AniketShirou <aniketranawork@gmail.com>
2025-08-20 02:22:28 +0530

    Cart and PlaceOrder Page

--- Frontend/src/context/ShopContext.jsx
+++ Frontend/src/context/ShopContext.jsx
@@ -1,6 +1,7 @@
-import React, { createContext, use, useEffect, useState } from "react";
+import React, { createContext, useEffect, useState } from "react";
 import { products } from "../assets/frontend_assets/assets";
 import { toast } from "react-toastify";
+import { useNavigate } from "react-router-dom";
 
 export const ShopContext = createContext();
 
@@ -11,6 +12,7 @@ const ShopContextProvider = (props) => {
    const [search , setSearch] = useState('');
    const [showSearch , setShowSearch] = useState(false);
    const [cartItems, setCartItems] = useState({});
+   const navigate = useNavigate();
 
    const addToCart = async (ItemId , size) =>{
 
@@ -60,9 +62,35 @@ const ShopContextProvider = (props) => {
       return count;
    }
 
+   const updateQuantity = (itemId, size, quantity) => {
+      let cartData = structuredClone(cartItems);
+      cartData[itemId][size] = quantity;
+
+      setCartItems(cartData);
+   }
+
+   const getCartAmount = () => {
+      let total = 0;
+      for(let items in cartItems){
+            let itemInfo = products.find((product) => product._id === items);
+            for(let size in cartItems[items]){
+               try{
+                  if(cartItems[items][size] > 0){
+                     total += itemInfo.price * cartItems[items][size];
+                  }
+               } catch(e){
+                  console.error("Error in getCartAmount: ", e);
+               }
+            }
+         }
+
+         return total;
+      }
+
+
    const value = {
         products , currency , delivery_fee, search, setSearch, showSearch, setShowSearch,
-        cartItems, addToCart, getCartCount
+        cartItems, addToCart, getCartCount, updateQuantity, getCartAmount, navigate
    }
 
    return(
@@ -70,6 +98,6 @@ const ShopContextProvider = (props) => {
         {props.children}
      </ShopContext.Provider>
    )
-}
+};
 
 export default ShopContextProvider;
