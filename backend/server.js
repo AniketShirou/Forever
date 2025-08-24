800f2840a8054652fcada7c29c6e823e307181ef
AniketShirou <aniketranawork@gmail.com>
2025-08-24 18:30:11 +0530

    Backend Setup

---
+++ backend/server.js
@@ -0,0 +1,24 @@
+import express from 'express';
+import cors from 'cors';
+import 'dotenv/config';
+import connectDB from './config/mongodb.js';
+
+
+//App config
+const app = express();
+const port = process.env.PORT || 8000;
+connectDB();
+
+
+//Middlewares
+app.use(express.json());
+app.use(cors());
+
+// api endpoints
+app.get('/', (req, res) => {
+    res.status(200).send('Hello World!')
+});
+
+app.listen(port, () => {
+    console.log(`Server Started on PORT:  ${port}`)
+});
